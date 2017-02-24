c20268681d2067019c29c54a1b78135e57544809
Albom <Albom@iion-19>
2017-02-24 13:47:49 +0200

    Some constants from physics added.

---
+++ src/main/java/com/albom/physics/Constants.java
@@ -0,0 +1,15 @@
+package com.albom.physics;
+
+public final class Constants {
+
+	public static final double SPEED_OF_LIGHT = 2.99792458e8;
+	public static final double EPSILON_0 = 8.854187817e-12;
+	public static final double BOLTZMANN = 1.3806504e-23;
+	public static final double ATOMIC_CONSTANT = 1.6605402e-27;
+	public static final double MASS_OF_ELECTRON = 9.10938188e-31;
+	public static final double CHARGE_OF_ELECTRON = -1.602176487e-19;
+
+	private Constants() {
+	}
+
+}
