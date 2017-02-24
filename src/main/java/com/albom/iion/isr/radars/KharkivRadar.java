1037e5f8a53a91503cd737490f02b523897ce1f6
Albom <Albom@iion-19>
2017-02-24 15:45:50 +0200

    Kharkiv ISR parameters added.

---
+++ src/main/java/com/albom/iion/isr/radars/KharkivRadar.java
@@ -0,0 +1,13 @@
+package com.albom.iion.isr.radars;
+
+import com.albom.physics.Constants;
+
+public final class KharkivRadar {
+
+	public final static double FREQUENCY = 158.0036e6;
+	public final static double WAVE_LENGTH = Constants.SPEED_OF_LIGHT / FREQUENCY;
+
+	private KharkivRadar() {
+	}
+
+}
