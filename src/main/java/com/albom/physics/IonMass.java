3875fb8fa1a5955b79d47e0572b4622d4c4a094a
Albom <Albom@iion-19>
2017-02-24 14:15:27 +0200

    Constants for ion mass added.

---
+++ src/main/java/com/albom/physics/IonMass.java
@@ -0,0 +1,12 @@
+package com.albom.physics;
+
+public class IonMass {
+	
+	public static final double HYDROGEN = 1;
+	public static final double HELIUM = 4;
+	public static final double OXYGEN = 16;
+	public static final double HEAVY_MOLECULAR = 30.5;
+
+	private IonMass() {
+	}
+}
