cfd379693a81f42b8422aaa0b1857efa2b20005d
Albom <albom85@yandex.ru>
2017-03-15 16:33:26 +0200

    IS signal spectrum calculation fixed

--- src/main/java/com/albom/application/TestApplication.java
+++ src/main/java/com/albom/application/TestApplication.java
@@ -1,29 +1,33 @@
 package com.albom.application;
 
-import com.albom.iion.isr.radars.KharkivRadar;
+import com.albom.iion.isr.processing.DirectProblem;
 
 public class TestApplication {
 
 	public static void main(String[] args) {
-//		 SNewFile s = new SNewFile();
-//		 SNewFileFS.loadFile(s, "d:/S190613.770");
-//		 System.out.print(s);
-//		ProjectFactory projectFactory = new ProjectFactory();
-//		ProjectDB project = projectFactory.getProject("d:/1.db3");
-//		System.out.print("OK");
+		// SNewFile s = new SNewFile();
+		// SNewFileFS.loadFile(s, "d:/S190613.770");
+		// System.out.print(s);
+		// ProjectFactory projectFactory = new ProjectFactory();
+		// ProjectDB project = projectFactory.getProject("d:/1.db3");
+		// System.out.print("OK");
 
-//		LocalDateTime[] t = new LocalDateTime[3];
-//		for (int i = 0; i < 3; i++){
-//			t[i] = LocalDateTime.now();
-//			t[i] = t[i].plusSeconds(-62*i);
-//			}
-//		
-		//TimeSorter tSort = new TimeSorter();
-//		tSort.sort(t);
-		//System.out.print(Arrays.toString(tSort.sort(t)));
-//		System.out.print(KharkivRadar.WAVE_LENGTH);
+		// LocalDateTime[] t = new LocalDateTime[3];
+		// for (int i = 0; i < 3; i++){
+		// t[i] = LocalDateTime.now();
+		// t[i] = t[i].plusSeconds(-62*i);
+		// }
+		//
+		// TimeSorter tSort = new TimeSorter();
+		// tSort.sort(t);
+		// System.out.print(Arrays.toString(tSort.sort(t)));
+		// System.out.print(KharkivRadar.WAVE_LENGTH);
 
-		
+		DirectProblem d = new DirectProblem();
+		for (int f = 0; f < 5000; f += 200) {
+			System.out.println(d.spectrum(0, 0, 1000, 1000, 1e5, false, f));
+		}
+
 	}
 
 }
--- src/main/java/com/albom/iion/isr/processing/DirectProblem.java
+++ src/main/java/com/albom/iion/isr/processing/DirectProblem.java
@@ -24,8 +24,8 @@ public class DirectProblem {
 		this.m1 = m1;
 		this.m2 = m2;
 		this.m3 = m3;
-		m2m1 = m2 / m1;
-		m3m1 = m3 / m1;
+		m2m1 = this.m2 / m1;
+		m3m1 = this.m3 / m1;
 		sqrt_m2m1 = Math.sqrt(m2m1);
 		sqrt_m3m1 = Math.sqrt(m3m1);
 		sqrt_m2m1_1000 = sqrt_m2m1 * 1000;
@@ -50,7 +50,25 @@ public class DirectProblem {
 	}
 
 	private static double[] calcPhi() {
-		double[] phi = new double[20000];
+
+		int length = 20000;
+		double step = 1e-3;
+
+		double[] phi = new double[length];
+
+		double step2 = step * step;
+
+		phi[0] = 0;
+
+		for (int t = 1; t < length; t++)
+			phi[t] = phi[t - 1] + Math.exp(t * t * step2);
+
+		for (int t = 0; t < length; t++)
+			phi[t] *= 2 * t * step2 * Math.exp(-t * t * step2);
+
+		for (int t = 0; t < length; t++)
+			phi[t] -= t * step2;
+
 		return phi;
 	}
 
@@ -72,12 +90,12 @@ public class DirectProblem {
 		}
 
 		double theta2 = theta * theta;
-		double theta_sqrt_pi = theta * Math.PI;
+		double theta_sqrt_pi = theta * Math.sqrt(Math.PI);
 		double t = te / ti;
 
 		int theta_int1 = (int) (theta * 1000);
-		int theta_int2 = theta * sqrt_m2m1 > 19.999 ? (int) (theta * sqrt_m2m1_1000) : 19999;
-		int theta_int3 = theta * sqrt_m3m1 > 19.999 ? (int) (theta * sqrt_m3m1_1000) : 19999;
+		int theta_int2 = theta * sqrt_m2m1 > 19.999 ? 19999 : (int) (theta * sqrt_m2m1_1000);
+		int theta_int3 = theta * sqrt_m3m1 > 19.999 ? 19999 : (int) (theta * sqrt_m3m1_1000);
 
 		double realYelect = theta_sqrt_pi * sqrt_memi * Math.pow(t, -0.5) * Math.exp(-memi * theta2 / t);
 		double modYelect2 = (1 + realYelect * realYelect);
@@ -92,8 +110,21 @@ public class DirectProblem {
 
 		double coeffDeby = (4 * Math.PI * waveLength) * (4 * Math.PI * waveLength) * Constants.EPSILON_0
 				* Constants.BOLTZMANN / (Constants.CHARGE_OF_ELECTRON * Constants.CHARGE_OF_ELECTRON);
-		double deby = coeffDeby * te / (ne > 1 ? ne : 1);
+		double deby = isDeby ? (coeffDeby * te / (ne > 1 ? ne : 1)) : 0;
 
-		return  (modYelect2*sumRealYion + (tSumRealYion*tSumRealYion + (tSumImagYion+ (isDeby ? deby : 0) )*(tSumImagYion+(isDeby ? deby : 0))) * realYelect ) / ( ((realYelect+tSumRealYion)*(realYelect+tSumRealYion) + (1+tSumImagYion+(isDeby ? deby : 0))*(1+tSumImagYion+(isDeby ? deby : 0)) )*theta ) ;
+		return (modYelect2 * sumRealYion
+				+ (tSumRealYion * tSumRealYion + (tSumImagYion + deby) * (tSumImagYion + deby)) * realYelect)
+				/ (((realYelect + tSumRealYion) * (realYelect + tSumRealYion)
+						+ (1 + tSumImagYion + deby) * (1 + tSumImagYion + deby)) * theta);
+
 	}
+
+	public double[] spectrum(double g1, double g2, double ti, double te, double ne, boolean isDeby, double freq[]) {
+		double[] result = new double[freq.length];
+		for (int f = 0; f < freq.length; f++) {
+			result[f] = spectrum(g1, g2, ti, te, ne, isDeby, f);
+		}
+		return result;
+	}
+
 }
