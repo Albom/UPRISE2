16a1a96173cfe343f9ae84f1ad2a557c646f9785
Albom <albom85@yandex.ru>
2017-03-16 12:58:05 +0200

    Millstone Hill ISR parameters

---
+++ src/main/java/com/albom/iion/isr/radars/MillstoneHillRadar.java
@@ -0,0 +1,13 @@
+package com.albom.iion.isr.radars;
+
+import com.albom.physics.Constants;
+
+public final class MillstoneHillRadar {
+
+	public final static double FREQUENCY = 440.2e6;
+	public final static double WAVE_LENGTH = Constants.SPEED_OF_LIGHT / FREQUENCY;
+
+	private MillstoneHillRadar() {
+	}
+
+}
