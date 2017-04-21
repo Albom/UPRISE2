c52978e7573da56cbf14f2a00ce8215717eefc2d
Albom <albom85@yandex.ru>
2017-04-21 12:43:43 +0300

    MU radar constants

---
+++ src/main/java/com/albom/iion/isr/radars/MURadar.java
@@ -0,0 +1,13 @@
+package com.albom.iion.isr.radars;
+
+import com.albom.physics.Constants;
+
+public class MURadar {
+
+	public final static double FREQUENCY = 46.5e6;
+	public final static double WAVE_LENGTH = Constants.SPEED_OF_LIGHT / FREQUENCY;
+	
+	private MURadar() {
+	}
+	
+}
