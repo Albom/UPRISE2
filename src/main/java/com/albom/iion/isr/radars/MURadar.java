d480e1f1ca0de795a876cfb27dfe329724d0ca5e
Albom <albom85@yandex.ru>
2017-10-10 14:48:14 +0300

    Distance between pulses added

--- src/main/java/com/albom/iion/isr/radars/MURadar.java
+++ src/main/java/com/albom/iion/isr/radars/MURadar.java
@@ -6,8 +6,10 @@ public class MURadar {
 
 	public final static double FREQUENCY = 46.5e6;
 	public final static double WAVE_LENGTH = Constants.SPEED_OF_LIGHT / FREQUENCY;
-	
+
+	public final static int DISTANCE_BETWEEN_PULSES = 192; // us
+
 	private MURadar() {
 	}
-	
+
 }
