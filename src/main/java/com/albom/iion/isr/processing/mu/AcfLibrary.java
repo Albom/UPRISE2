e8eefe500446dcd79d5154eb9820a2159c68585f
Albom <albom85@yandex.ru>
2018-04-06 12:51:17 +0300

    Just current state of work

--- src/main/java/com/albom/iion/isr/processing/mu/AcfLibrary.java
+++ src/main/java/com/albom/iion/isr/processing/mu/AcfLibrary.java
@@ -1,9 +1,7 @@
 package com.albom.iion.isr.processing.mu;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
 import com.albom.iion.isr.processing.ForwardProblem;
 import com.albom.iion.isr.radars.MURadar;
