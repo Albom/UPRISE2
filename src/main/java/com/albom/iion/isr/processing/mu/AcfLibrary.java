00f5976a79e0d0d864c784f9d80c1219b26e1e16
Albom <albom85@yandex.ru>
2018-07-06 14:32:07 +0300

    class Offset changed

--- src/main/java/com/albom/iion/isr/processing/mu/AcfLibrary.java
+++ src/main/java/com/albom/iion/isr/processing/mu/AcfLibrary.java
@@ -1,35 +1,24 @@
 package com.albom.iion.isr.processing.mu;
 
 import java.util.HashMap;
-import java.util.Map;
 
 import com.albom.iion.isr.processing.ForwardProblem;
 import com.albom.iion.isr.radars.MURadar;
 import com.albom.physics.UnitPrefix;
 
 public class AcfLibrary {
-	
-	private class Offset {
-		
-		private int ti = 0;
-		private int te = 0;
-		private int offset = 0;
-		
-		public Offset(int ti, int te, int offset){
-			this.ti = ti;
-			this.te = te;
-			this.offset = offset;
-		}
 
+	private class Offset extends HashMap<Long, Integer> {
+		private static final long serialVersionUID = 1L;
 	}
-
+	
 	private final int LENGTH = 7;
 	private int num = 0;
 
 	private double[] acfs = null;
 
-	private Map<Long, Offset> offsets = new HashMap<Long, Offset>();
-	
+	Offset offsets = new Offset();
+
 	public AcfLibrary(int tMin, int tMax, int step) {
 
 		calcNum(tMin, tMax, step);
@@ -46,11 +35,11 @@ public class AcfLibrary {
 				for (int lag = 0; lag < LENGTH; lag++) {
 					acfs[lag + pos * LENGTH] = acf[lag];
 				}
-				offsets.put((long) ti << 32 | (long) te, new Offset(ti, te, pos * LENGTH));
+				offsets.put((long) ti << 32 | (long) te, pos * LENGTH);
 				pos++;
 			}
 		}
-		
+
 	}
 
 	private void calcNum(int tMin, int tMax, int step) {
@@ -62,19 +51,19 @@ public class AcfLibrary {
 	}
 
 	/**
-	 * @return the num
+	 * @return num
 	 */
 	public int getNum() {
 		return num;
 	}
 
 	public boolean getAcf(int ti, int te, double acf[]) {
-		Offset offset = offsets.get((long) ti << 32 | (long) te);
+		Integer offset = offsets.get((long) ti << 32 | (long) te);
 		if (offset == null) {
 			return false;
 		}
 		for (int lag = 0; lag < LENGTH; lag++) {
-			acf[lag] = acfs[lag + offset.offset];
+			acf[lag] = acfs[lag + offset];
 		}
 		return true;
 	}
